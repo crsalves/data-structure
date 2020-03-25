251f79d9ef04392bfa59dd25a818e77d1473ee20
crsalves <carlaalves.crs@gmail.com>
2020-03-25 15:32:08 -0400

    First commit

---
+++ prjLists/prjLists/Linear_Sequential_List.c
@@ -0,0 +1,23 @@
+/*!	\file		Linear_Sequential_List.c
+	\author		Carla Alves
+	\date		2020-03-25
+
+	Implementation of a linear list data structure with auxiliary functions:
+	- Initialization;
+	- Display all elements;
+	- Total size;
+	- Return first element;
+	- Return last element;
+	- Sequential searh;
+	- Insert one element;
+	- Delete one element;
+	- Reinitialization;
+*/
+
+#include"stdio.h"
+
+int main() {
+
+
+	return 0;
+}
