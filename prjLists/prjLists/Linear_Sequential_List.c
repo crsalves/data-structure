f2b8d655635a1ff555b2b76c4c8415d722a11d0e
crsalves <carlaalves.crs@gmail.com>
2020-03-30 10:26:45 -0400

    Library

--- prjLists/prjLists/Linear_Sequential_List.c
+++ prjLists/prjLists/Linear_Sequential_List.c
@@ -1,160 +1,31 @@
-///*!	\file		Linear_Sequential_List.c
-//	\author		Carla Alves
-//	\date		2020-03-25
-//
-//	Implementation of a linear list data structure using auxiliary functions:
-//	1) Initialization;
-//	2) Display all elements;
-//	3) Total elements;
-//	4) Return the first element;
-//	5) Return the last element;
-//	6) Sequential search;
-//	7) Insert one element;
-//	8) Delete one element;
-//	9) Reinitialization;
-//*/
-//
-//#include"stdio.h"
-//#include"stdbool.h"
-//#define MAX 50
-//
-//typedef int KEYTYPE;
-//
-//typedef struct {
-//	KEYTYPE key;
-//}Register;
-//
-//typedef struct {
-//	Register tabElem[MAX];
-//	int nbElem;
-//}List;
-//
-///* 
-//	1) Initializing the list
-//	We must use a pointer as a parameter, otherwise the list will not be updated.
-//*/
-//void initializeSequentialList(List * anyList) {
-//	anyList->nbElem = 0;
-//}
-//
-///* 
-//	2) Displaying all elements stored in the list
-//*/
-//void displayList(List* anyList) {
-//	for(int indx = 0; indx < anyList->nbElem; indx++)
-//		printf("[%i] = %i\n", indx, anyList->tabElem[indx].key);
-//}
-//
-///*
-//	3)  Finding the total number of elements stored in the list
-//*/
-//int findNbElements(List* anyList) {
-//	return anyList->nbElem;
-//}
-//
-///*
-//	4) Getting the first value stored in the list
-//*/
-//KEYTYPE getFirstElement(List* anyList) {
-//	if (anyList->nbElem > 0)
-//		return (anyList->tabElem[0].key);
-//	else
-//		return (-1); //empty list
-//}
-//
-///*
-//	5) Getting the last value stored in the list
-//*/
-//KEYTYPE getLastElement(List* anyList) {
-//	if (anyList->nbElem > 0)
-//		return (anyList->tabElem[anyList->nbElem-1].key);
-//	else
-//		return (-1); //empty list
-//}
-//
-///*
-//	6) Searching in the sequential list
-//*/
-//int searchSequentialList(List* anyList, KEYTYPE anyKey) {
-//	int indx = 0;
-//	while (indx < anyList->nbElem) {
-//		if (anyKey == anyList->tabElem[indx].key)
-//			return indx;// Element Found!
-//		indx++;
-//	}
-//	return -1;// Element not found!
-//}
-//
-///*
-//	7) Inserting one element in a specific position
-//		*Defining features:
-//		1. If the list is not full and it is a valid position (index)
-//			1.1 All previous elements will be placed/copy to the right
-//			1.2 Insert/assign the element in the desired position
-//			1.3 Increment nbElement
-//			1.4 return true
-//		1.1 Else 
-//			1.1.1 Return false
-//*/
-//bool insertElemList(List* anyList, Register anyReg, int pos) {
-//	int indx;
-//
-//	if ((anyList->nbElem == MAX) || (pos < 0) || pos > anyList->nbElem)
-//		return false;
-//
-//	//	In order to release position and replace the elements to the 
-//	// right index while searching, it searches the "pos" from back to front.
-//	for (indx = anyList->nbElem; indx > pos; indx--) {
-//		anyList->tabElem[indx] = anyList->tabElem[indx - 1];
-//	}
-//		
-//	anyList->tabElem[pos] = anyReg;
-//	anyList->nbElem++;
-//	return true;
-//}
-//
-///*
-//	8) Excluding one specific element
-//		*Defining method:
-//		1. Search the element
-//		2. Move one position back all elements in the right side
-//		3. Decrement nbElements
-//*/
-//bool deleteElemList(List* anyList, KEYTYPE anyKey) {
-//	int pos = searchSequentialList(anyList, anyKey);
-//
-//	if (pos == -1)
-//		return false;
-//
-//	// It will overwrite the elements, "deleting them"
-//	for (int indx = pos; indx < anyList->nbElem; indx++)
-//		anyList->tabElem[indx] = anyList->tabElem[indx + 1];
-//
-//	// It "disables" access to the remainder last value, "deleting it"
-//	anyList->nbElem--;
-//
-//	return true;
-//}
-//
-///*
-//	9) Reinitializing the list
-//*/
-//void reinitializeSequentialList(List* anyList) {
-//	anyList->nbElem = 0;
-//}
-//
-//
-//int mainList() {
-//
-//	List myList;
-//	initializeSequentialList(&myList);
-//
-//	Register crrYear;
-//	crrYear.key = 2020;
-//	insertElemList(&myList, crrYear, 0);
-//	crrYear.key = 500;
-//	insertElemList(&myList, crrYear, 1);
-//	displayList(&myList);
-//
-//	return 0;
-//}
+/*!	\file		Linear_Sequential_List.c
+	\author		Carla Alves
+	\date		2020-03-25
+
+	Implementation of a linear list data structure using auxiliary functions:
+	1) Initialization;
+	2) Display all elements;
+	3) Total elements;
+	4) Return the first element;
+	5) Return the last element;
+	6) Sequential search;
+	7) Insert one element;
+	8) Delete one element;
+	9) Reinitialization;
+*/
+#include"listLib.h"
+
+int main() {
+
+	List myList;
+	initializeSequentialList(&myList);
+
+	Register crrYear;
+	crrYear.key = 2020;
+	insertElemList(&myList, crrYear, 0);
+	crrYear.key = 500;
+	insertElemList(&myList, crrYear, 1);
+	displayList(&myList);
+
+	return 0;
+}
--- prjLists/prjLists/Linked_List_Circular.c
+++ prjLists/prjLists/Linked_List_Circular.c
@@ -1,239 +1,239 @@
-/*!	\file		Linked_List_Circular.c
-	\author		Carla Alves
-	\date		2020-03-29
-
-	Dinamic implamentation of a linked linear list:
-	1) Physically, the elements are spreadly stored in the computer memory;
-	2) All elements have the address of the 'next element position', except the last element;
-	3) It is necessary to know the 'first element position';
-	4) Every new element, it allocates a space in memory dinamically;
-*/
-
-#include"stdio.h"
-#include"stdbool.h"
-#include"malloc.h"
-
-typedef int KEYTYPE;
-
-typedef struct {
-	KEYTYPE key;
-}Register;
-
-typedef struct tmpReg {
-	Register reg;
-	struct tmpReg* nextPos;
-}Element;
-
-typedef Element* POINTER;
-
-typedef struct {
-	POINTER head;
-}List;
-
-/*
-	1) Initializing the list
-*/
-void initializeLinkListCircular(List* anyList) {
-	anyList->head = (POINTER)malloc(sizeof(Element));
-	anyList->head->nextPos = anyList->head;
-}
-    
-/*
-	2) Displaying all elements stored in the list
-*/
-void displayLinkedListCircular(List* anyList) {
-	POINTER end = anyList->head->nextPos;
-	int counter = 0;// Just for view purposes (to show the index for the user)
-	while (end != anyList->head) {
-		printf("[%i] = %i\n", counter, end->reg.key);
-		end = end->nextPos;
-		counter++;
-	}
-}
-
-/*
-	3)  Finding the total number of elements stored in the list
-*/
-int findNbElemLinkListCircular(List* anyList) {
-	POINTER end = anyList->head->nextPos;
-	int listSize = 0;
-
-	while (end != anyList->head) {
-		listSize++;
-		end = end->nextPos;
-	}
-	return listSize;
-}
-
-/*
-	4) Getting the first value stored in the list
-*/
-KEYTYPE getFstElemLinkListCircular(List* anyList) {
-	int length = findNbElemLinkListCircular(anyList);
-	if (length > 0) {
-		return (anyList->head->nextPos->reg.key);
-	}
-	else
-		return (-1); //empty list
-}
-
-/*
-	5) Getting the last value stored in the list
-*/
-int getLstElemLinkListCircular(List* anyList) {
-	POINTER end = anyList->head->nextPos;
-	while (end != anyList->head) {		
-		if (end->nextPos == anyList->head)
-			return end->reg.key;
-		end = end->nextPos;
-	}
-	return -1; //empty list
-}
-
-/*
-	6) Searching in the linked list ordered using 'Sentinel'
-		**1st APPROACH: Unordered list
-*/
-POINTER searchSeqLinkListUnordCircular(List* anyList, KEYTYPE anyKey) {
-	POINTER pos = anyList->head->nextPos;
-	anyList->head->reg.key = anyKey;
-
-	while (pos->reg.key != anyKey) {
-		pos = pos->nextPos;
-	}
-
-	// Checking if it reached the sentinel
-	if (pos != anyList->head)
-		return pos;
-	return NULL; // Not found!
-}
-
-/*   
-	6) Searching in the linked list ordered using 'Sentinel'
-		**2nd APPROACH: Ordered list
-*/
-POINTER searchSeqLinkListOrdCircular(List* anyList, KEYTYPE anyKey) {
-	POINTER pos = anyList->head->nextPos;
-	anyList->head->reg.key = anyKey;
-
-	while (pos->reg.key < anyKey) {
-		pos = pos->nextPos;
-	}
-
-	// Checking if it reached the sentinel
-	if ((pos != anyList->head) && (pos->reg.key == anyKey))
-		return pos;
-	return NULL; // Not found!
-}
-
-/*
-	**Auxiliary function for insertation
-
-	Note! Even it doesn't find the searched value, it always keeps the address of the PREDECESSOR element
-*/
-int getAvailablePosLinkListCircular(List* anyList, KEYTYPE anyKey, POINTER* anyPred) {
-	*anyPred = anyList->head;
-	POINTER current = anyList->head->nextPos;
-	anyList->head->reg.key = anyKey;
-
-	// Checking values to store neatly
-	while (current->reg.key < anyKey) {
-		*anyPred = current;
-		current = current->nextPos;
-	}
-
-	if ((current != anyList->head) && (current->reg.key == anyKey))
-		return current;
-	return NULL;// Not found!
-}
-
-/*
-	7) Inserting elements in ascending position
-	*It must be ordered;
-	*No repeated values;
-
-	**Auxiliary func to get the available position
-	and update the available position for the next turn;
-*/
-bool insertElemLinkListOrdCircular(List* anyList, Register anyReg) {
-	POINTER nPred, nAddress;
-
-	// Call the auxiliary func
-	nAddress = getAvailablePosLinkListCircular(anyList, anyReg.key, &nPred);
-
-	// Checking for repeated values
-	if (nAddress != NULL)
-		return false;
-
-	// Allocating memory
-	nAddress = (POINTER)malloc(sizeof(Element));
-	nAddress->reg = anyReg;
-
-	// It is not necessary to test (if-else) because it will always have a predecessor value. 
-	// Also, it updates the next value to maintain the next position of the next element.
-	nAddress->nextPos = nPred->nextPos;
-	nPred->nextPos = nAddress;
-	return true;
-}
-
-
-/*
-	8) Excluding one specific element
-
-	**Auxiliary func to search the position of the excluded element;
-	*** Always free the memory space!!!!
-*/
-bool deleteElemLinkListCircular(List* anyList, KEYTYPE anyKey) {
-	POINTER nPred, nAddress;
-	nAddress = getAvailablePosLinkListCircular(anyList, anyKey, &nPred);
-
-	// Checking if the element exist in the list
-	if (nAddress == NULL)
-		return false;
-
-	nPred->nextPos = nAddress->nextPos;
-
-	free(nAddress);
-	return true;
-}
-
-/*
-	9) Reinitializing the list
-	*** Always free the memory space!!!!
-*/
-void reinitializeSeqLinkListCircular(List* anyList) {
-	POINTER end = anyList->head->nextPos;
-
-	while (end != anyList->head) {
-		POINTER delete = end;
-		end = end->nextPos;
-		free(delete);
-	}
-	anyList->head->nextPos = anyList->head;
-}
-
-
-int main() {
-
-	List myList;
-	initializeLinkListCircular(&myList);
-
-	Register crrYear;
-	crrYear.key = 20;
-	insertElemLinkListOrdCircular(&myList, crrYear);
-	crrYear.key = 10;
-	insertElemLinkListOrdCircular(&myList, crrYear);
-	crrYear.key = 30;
-	insertElemLinkListOrdCircular(&myList, crrYear);
-	crrYear.key = 40;
-	insertElemLinkListOrdCircular(&myList, crrYear);
-	displayLinkedListCircular(&myList);
-
-	crrYear.key = 30;
-	printf("%i\n", searchSeqLinkListOrdCircular(&myList, crrYear.key));
-	printf("First element is %i\n", getFstElemLinkListCircular(&myList));
-	printf("Last element is %i\n", getLstElemLinkListCircular(&myList));
-
-	reinitializeSeqLinkListCircular(&myList);
-	return 0;
-}
+///*!	\file		Linked_List_Circular.c
+//	\author		Carla Alves
+//	\date		2020-03-29
+//
+//	Dinamic implamentation of a linked linear list:
+//	1) Physically, the elements are spreadly stored in the computer memory;
+//	2) All elements have the address of the 'next element position', except the last element;
+//	3) It is necessary to know the 'first element position';
+//	4) Every new element, it allocates a space in memory dinamically;
+//*/
+//
+//#include"stdio.h"
+//#include"stdbool.h"
+//#include"malloc.h"
+//
+//typedef int KEYTYPE;
+//
+//typedef struct {
+//	KEYTYPE key;
+//}Register;
+//
+//typedef struct tmpReg {
+//	Register reg;
+//	struct tmpReg* nextPos;
+//}Element;
+//
+//typedef Element* POINTER;
+//
+//typedef struct {
+//	POINTER head;
+//}List;
+//
+///*
+//	1) Initializing the list
+//*/
+//void initializeLinkListCircular(List* anyList) {
+//	anyList->head = (POINTER)malloc(sizeof(Element));
+//	anyList->head->nextPos = anyList->head;
+//}
+//    
+///*
+//	2) Displaying all elements stored in the list
+//*/
+//void displayLinkedListCircular(List* anyList) {
+//	POINTER end = anyList->head->nextPos;
+//	int counter = 0;// Just for view purposes (to show the index for the user)
+//	while (end != anyList->head) {
+//		printf("[%i] = %i\n", counter, end->reg.key);
+//		end = end->nextPos;
+//		counter++;
+//	}
+//}
+//
+///*
+//	3)  Finding the total number of elements stored in the list
+//*/
+//int findNbElemLinkListCircular(List* anyList) {
+//	POINTER end = anyList->head->nextPos;
+//	int listSize = 0;
+//
+//	while (end != anyList->head) {
+//		listSize++;
+//		end = end->nextPos;
+//	}
+//	return listSize;
+//}
+//
+///*
+//	4) Getting the first value stored in the list
+//*/
+//KEYTYPE getFstElemLinkListCircular(List* anyList) {
+//	int length = findNbElemLinkListCircular(anyList);
+//	if (length > 0) {
+//		return (anyList->head->nextPos->reg.key);
+//	}
+//	else
+//		return (-1); //empty list
+//}
+//
+///*
+//	5) Getting the last value stored in the list
+//*/
+//int getLstElemLinkListCircular(List* anyList) {
+//	POINTER end = anyList->head->nextPos;
+//	while (end != anyList->head) {		
+//		if (end->nextPos == anyList->head)
+//			return end->reg.key;
+//		end = end->nextPos;
+//	}
+//	return -1; //empty list
+//}
+//
+///*
+//	6) Searching in the linked list ordered using 'Sentinel'
+//		**1st APPROACH: Unordered list
+//*/
+//POINTER searchSeqLinkListUnordCircular(List* anyList, KEYTYPE anyKey) {
+//	POINTER pos = anyList->head->nextPos;
+//	anyList->head->reg.key = anyKey;
+//
+//	while (pos->reg.key != anyKey) {
+//		pos = pos->nextPos;
+//	}
+//
+//	// Checking if it reached the sentinel
+//	if (pos != anyList->head)
+//		return pos;
+//	return NULL; // Not found!
+//}
+//
+///*   
+//	6) Searching in the linked list ordered using 'Sentinel'
+//		**2nd APPROACH: Ordered list
+//*/
+//POINTER searchSeqLinkListOrdCircular(List* anyList, KEYTYPE anyKey) {
+//	POINTER pos = anyList->head->nextPos;
+//	anyList->head->reg.key = anyKey;
+//
+//	while (pos->reg.key < anyKey) {
+//		pos = pos->nextPos;
+//	}
+//
+//	// Checking if it reached the sentinel
+//	if ((pos != anyList->head) && (pos->reg.key == anyKey))
+//		return pos;
+//	return NULL; // Not found!
+//}
+//
+///*
+//	**Auxiliary function for insertation
+//
+//	Note! Even it doesn't find the searched value, it always keeps the address of the PREDECESSOR element
+//*/
+//int getAvailablePosLinkListCircular(List* anyList, KEYTYPE anyKey, POINTER* anyPred) {
+//	*anyPred = anyList->head;
+//	POINTER current = anyList->head->nextPos;
+//	anyList->head->reg.key = anyKey;
+//
+//	// Checking values to store neatly
+//	while (current->reg.key < anyKey) {
+//		*anyPred = current;
+//		current = current->nextPos;
+//	}
+//
+//	if ((current != anyList->head) && (current->reg.key == anyKey))
+//		return current;
+//	return NULL;// Not found!
+//}
+//
+///*
+//	7) Inserting elements in ascending position
+//	*It must be ordered;
+//	*No repeated values;
+//
+//	**Auxiliary func to get the available position
+//	and update the available position for the next turn;
+//*/
+//bool insertElemLinkListOrdCircular(List* anyList, Register anyReg) {
+//	POINTER nPred, nAddress;
+//
+//	// Call the auxiliary func
+//	nAddress = getAvailablePosLinkListCircular(anyList, anyReg.key, &nPred);
+//
+//	// Checking for repeated values
+//	if (nAddress != NULL)
+//		return false;
+//
+//	// Allocating memory
+//	nAddress = (POINTER)malloc(sizeof(Element));
+//	nAddress->reg = anyReg;
+//
+//	// It is not necessary to test (if-else) because it will always have a predecessor value. 
+//	// Also, it updates the next value to maintain the next position of the next element.
+//	nAddress->nextPos = nPred->nextPos;
+//	nPred->nextPos = nAddress;
+//	return true;
+//}
+//
+//
+///*
+//	8) Excluding one specific element
+//
+//	**Auxiliary func to search the position of the excluded element;
+//	*** Always free the memory space!!!!
+//*/
+//bool deleteElemLinkListCircular(List* anyList, KEYTYPE anyKey) {
+//	POINTER nPred, nAddress;
+//	nAddress = getAvailablePosLinkListCircular(anyList, anyKey, &nPred);
+//
+//	// Checking if the element exist in the list
+//	if (nAddress == NULL)
+//		return false;
+//
+//	nPred->nextPos = nAddress->nextPos;
+//
+//	free(nAddress);
+//	return true;
+//}
+//
+///*
+//	9) Reinitializing the list
+//	*** Always free the memory space!!!!
+//*/
+//void reinitializeSeqLinkListCircular(List* anyList) {
+//	POINTER end = anyList->head->nextPos;
+//
+//	while (end != anyList->head) {
+//		POINTER delete = end;
+//		end = end->nextPos;
+//		free(delete);
+//	}
+//	anyList->head->nextPos = anyList->head;
+//}
+//
+//
+//int main() {
+//
+//	List myList;
+//	initializeLinkListCircular(&myList);
+//
+//	Register crrYear;
+//	crrYear.key = 20;
+//	insertElemLinkListOrdCircular(&myList, crrYear);
+//	crrYear.key = 10;
+//	insertElemLinkListOrdCircular(&myList, crrYear);
+//	crrYear.key = 30;
+//	insertElemLinkListOrdCircular(&myList, crrYear);
+//	crrYear.key = 40;
+//	insertElemLinkListOrdCircular(&myList, crrYear);
+//	displayLinkedListCircular(&myList);
+//
+//	crrYear.key = 30;
+//	printf("%i\n", searchSeqLinkListOrdCircular(&myList, crrYear.key));
+//	printf("First element is %i\n", getFstElemLinkListCircular(&myList));
+//	printf("Last element is %i\n", getLstElemLinkListCircular(&myList));
+//
+//	reinitializeSeqLinkListCircular(&myList);
+//	return 0;
+//}
