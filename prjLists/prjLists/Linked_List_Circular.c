d72fed093a05389089207a035b6ca9f3e74a4d69
crsalves <carlaalves.crs@gmail.com>
2020-03-30 08:46:01 -0400

    Get first element

--- prjLists/prjLists/Linked_List_Circular.c
+++ prjLists/prjLists/Linked_List_Circular.c
@@ -27,24 +27,24 @@ typedef struct tmpReg {
 typedef Element* POINTER;
 
 typedef struct {
-	POINTER firstPos;
+	POINTER head;
 }List;
 
 /*
 	1) Initializing the list
 */
 void initializeLinkListCircular(List* anyList) {
-	anyList->firstPos = (POINTER)malloc(sizeof(Element));
-	anyList->firstPos->nextPos = anyList->firstPos;
+	anyList->head = (POINTER)malloc(sizeof(Element));
+	anyList->head->nextPos = anyList->head;
 }
-
+    
 /*
 	2) Displaying all elements stored in the list
 */
 void displayLinkedListCircular(List* anyList) {
-	POINTER end = anyList->firstPos->nextPos;
+	POINTER end = anyList->head->nextPos;
 	int counter = 0;// Just for view purposes (to show the index for the user)
-	while (end != anyList->firstPos) {
+	while (end != anyList->head) {
 		printf("[%i] = %i\n", counter, end->reg.key);
 		end = end->nextPos;
 		counter++;
@@ -55,10 +55,10 @@ void displayLinkedListCircular(List* anyList) {
 	3)  Finding the total number of elements stored in the list
 */
 int findNbElemLinkListCircular(List* anyList) {
-	POINTER end = anyList->firstPos->nextPos;
+	POINTER end = anyList->head->nextPos;
 	int listSize = 0;
 
-	while (end != NULL) {
+	while (end != anyList->head) {
 		listSize++;
 		end = end->nextPos;
 	}
@@ -71,7 +71,7 @@ int findNbElemLinkListCircular(List* anyList) {
 KEYTYPE getFstElemLinkListCircular(List* anyList) {
 	int length = findNbElemLinkListCircular(anyList);
 	if (length > 0) {
-		return (anyList->firstPos->reg.key);
+		return (anyList->head->nextPos->reg.key);
 	}
 	else
 		return (-1); //empty list
@@ -81,13 +81,14 @@ KEYTYPE getFstElemLinkListCircular(List* anyList) {
 	5) Getting the last value stored in the list
 */
 int getLstElemLinkListCircular(List* anyList) {
-	POINTER end = anyList->firstPos->nextPos->reg.key;
-	while (end != anyList->firstPos->reg.key) {
-		end = end->nextPos;
-		if (end->nextPos->reg.key == anyList->firstPos->reg.key)
+	POINTER end = anyList->head->nextPos;
+	while (end->nextPos != anyList->head) {
+		
+		if (end->nextPos == anyList->head)
 			return end->reg.key;
+		end->nextPos++;
 	}
-	return (-1); //empty list
+	return end->reg.key; //empty list
 }
 
 /*
@@ -95,15 +96,15 @@ int getLstElemLinkListCircular(List* anyList) {
 		**1st APPROACH: Unordered list
 */
 POINTER searchSeqLinkListUnordCircular(List* anyList, KEYTYPE anyKey) {
-	POINTER pos = anyList->firstPos->nextPos;
-	anyList->firstPos->reg.key = anyKey;
+	POINTER pos = anyList->head->nextPos;
+	anyList->head->reg.key = anyKey;
 
 	while (pos->reg.key != anyKey) {
 		pos = pos->nextPos;
 	}
 
 	// Checking if it reached the sentinel
-	if (pos != anyList->firstPos)
+	if (pos != anyList->head)
 		return pos;
 	return NULL; // Not found!
 }
@@ -112,16 +113,16 @@ POINTER searchSeqLinkListUnordCircular(List* anyList, KEYTYPE anyKey) {
 	6) Searching in the linked list ordered using 'Sentinel'
 		**2nd APPROACH: Ordered list
 */
-int searchSeqLinkListOrdCircular(List* anyList, KEYTYPE anyKey) {
-	POINTER pos = anyList->firstPos->nextPos;
-	anyList->firstPos->reg.key = anyKey;
+POINTER searchSeqLinkListOrdCircular(List* anyList, KEYTYPE anyKey) {
+	POINTER pos = anyList->head->nextPos;
+	anyList->head->reg.key = anyKey;
 
 	while (pos->reg.key < anyKey) {
 		pos = pos->nextPos;
 	}
 
 	// Checking if it reached the sentinel
-	if ((pos != anyList->firstPos) && (pos->reg.key == anyKey))
+	if ((pos != anyList->head) && (pos->reg.key == anyKey))
 		return pos;
 	return NULL; // Not found!
 }
@@ -132,9 +133,9 @@ int searchSeqLinkListOrdCircular(List* anyList, KEYTYPE anyKey) {
 	Note! Even it doesn't find the searched value, it always keeps the address of the PREDECESSOR element
 */
 int getAvailablePosLinkListCircular(List* anyList, KEYTYPE anyKey, POINTER* anyPred) {
-	*anyPred = anyList->firstPos;
-	POINTER current = anyList->firstPos->nextPos;
-	anyList->firstPos->reg.key = anyKey;
+	*anyPred = anyList->head;
+	POINTER current = anyList->head->nextPos;
+	anyList->head->reg.key = anyKey;
 
 	// Checking values to store neatly
 	while (current->reg.key < anyKey) {
@@ -142,7 +143,7 @@ int getAvailablePosLinkListCircular(List* anyList, KEYTYPE anyKey, POINTER* anyP
 		current = current->nextPos;
 	}
 
-	if ((current != anyList->firstPos) && (current->reg.key == anyKey))
+	if ((current != anyList->head) && (current->reg.key == anyKey))
 		return current;
 	return NULL;// Not found!
 }
@@ -202,14 +203,14 @@ bool deleteElemLinkListCircular(List* anyList, KEYTYPE anyKey) {
 	*** Always free the memory space!!!!
 */
 void reinitializeSeqLinkListCircular(List* anyList) {
-	POINTER end = anyList->firstPos->nextPos;
+	POINTER end = anyList->head->nextPos;
 
-	while (end != anyList->firstPos) {
+	while (end != anyList->head) {
 		POINTER delete = end;
 		end = end->nextPos;
 		free(delete);
 	}
-	anyList->firstPos->nextPos = anyList->firstPos;
+	anyList->head->nextPos = anyList->head;
 }
 
 
@@ -219,20 +220,20 @@ int main() {
 	initializeLinkListCircular(&myList);
 
 	Register crrYear;
-	crrYear.key = 40;
+	crrYear.key = 20;
 	insertElemLinkListOrdCircular(&myList, crrYear);
 	crrYear.key = 10;
 	insertElemLinkListOrdCircular(&myList, crrYear);
 	crrYear.key = 30;
 	insertElemLinkListOrdCircular(&myList, crrYear);
-	crrYear.key = 20;
+	crrYear.key = 40;
 	insertElemLinkListOrdCircular(&myList, crrYear);
 	displayLinkedListCircular(&myList);
 
 	crrYear.key = 20;
 	printf("%i\n", searchSeqLinkListOrdCircular(&myList, crrYear.key));
-	//printf("First element is %i\n", getFstElemLinkListCircular(&myList));
-	//printf("Last element is %i\n", getLstElemLinkListCircular(&myList));
+	printf("First element is %i\n", getFstElemLinkListCircular(&myList));
+	printf("Last element is %i\n", getLstElemLinkListCircular(&myList));
 
 	reinitializeSeqLinkListCircular(&myList);
 	return 0;
