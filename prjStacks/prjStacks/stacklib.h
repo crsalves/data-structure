b997f257feb99d1c0bda5b3b32bb22fcb74e9690
crsalves <carlaalves.crs@gmail.com>
2020-04-16 10:45:15 -0400

    Dynamic Stack

--- prjStacks/prjStacks/stacklib.h
+++ prjStacks/prjStacks/stacklib.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<stdio.h>
+#include<malloc.h>
 
 #define MAX 50
 
@@ -12,24 +13,59 @@ typedef struct {
 	KEYTYPE key;
 }Register;
 
+typedef struct aux {
+	Register reg;
+	struct aux* next;
+}Element;
+
+typedef Element* POINTER;
+
+typedef struct {
+	POINTER top;
+}DynamicStack;
+
 typedef struct {
 	Register tabElem[MAX];
 	int top;
-}Stack;
+}StaticStack;
 
 // Initialization 
-void initialize(Stack* nStack) {
+void initializeStaticStack(StaticStack* nStack) {
 	nStack->top = -1;
 }
 
+// Initialization 
+void initializeDynamicStack(DynamicStack* nStack) {
+	nStack->top = NULL;
+}
+
+// Numbers of elements (Size)
+int findNbElemDynamicStack(DynamicStack* nStack) {
+	POINTER end = nStack->top;
+	int nbElem = 0;
+	while (end != NULL) {
+		nbElem++;
+		end = end->next;
+	}
+	return nbElem;
+}
+
 // Exibition
-void displayStack(Stack* nStack) {
+void displayStaticStack(StaticStack* nStack) {
 	for (int indx = nStack->top; indx >= 0; indx--)
 		printf("%i \n", nStack->tabElem[indx].key);
 }
 
+void displayDynamicStack(DynamicStack* nStack) {
+	POINTER end = nStack->top;
+	while (end != NULL) {
+		printf("%i \n", end->reg.key);
+		end = end->next;
+	}
+}
+
 // Inserting elements (PUSH)
-bool pushStaticStack(Stack* nStack, Register nReg) {
+bool pushStaticStack(StaticStack* nStack, Register nReg) {
 	if (nStack->top >= MAX - 1)// Test for available position
 		return false; // It is full!
 
@@ -38,8 +74,17 @@ bool pushStaticStack(Stack* nStack, Register nReg) {
 	return true;
 }
 
+// Inserting elements (PUSH)
+bool pushDynamicStack(DynamicStack* nStack, Register nReg) {
+	POINTER newElem = (POINTER)malloc(sizeof(Element));
+	newElem->reg = nReg;
+	newElem->next = nStack->top;
+	nStack->top = newElem;
+	return true;
+}
+
 // Excluding/Deleting (POP)
-bool popStaticStack(Stack* nStack, Register* nReg) {
+bool popStaticStack(StaticStack* nStack, Register* nReg) {
 	if (nStack->top == -1)
 		return false;
 
@@ -48,7 +93,32 @@ bool popStaticStack(Stack* nStack, Register* nReg) {
 	return true;
 }
 
+// Excluding/Deleting (POP)
+bool popDynamicStack(DynamicStack* nStack, Register* nReg) {
+	if (nStack->top == NULL)
+		return false;
+
+	*nReg = nStack->top->reg;
+	POINTER delElem = nStack->top;
+	nStack->top = nStack->top->next;
+	free(delElem);
+	return true;
+}
+
 // Reinicialization
-void restartStaticStack(Stack* nStack) {
+void restartStaticStack(StaticStack* nStack) {
 	nStack->top = -1;
+}
+
+// Reinicialization
+void restartDynamicStack(DynamicStack* nStack) {
+	POINTER delElem;
+	POINTER position = nStack->top;
+
+	while (position != NULL) {
+		delElem = position;
+		position = position->next;
+		free(delElem);
+	}
+	nStack->top = NULL;
 }
